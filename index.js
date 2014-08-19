5b237b3696d17ba26810c8981e036195c29c7724
Brady Garvin <brady.j.garvin@gmail.com>
2014-08-18 21:00:00 -0500

    Added jsLint directive and clarified JavaScript comments.

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-"use strict"; // This line chooses a JavaScript dialect, one that helps OrionHub catch errors.
+"use strict"; // This line chooses a JavaScript dialect, one that helps both jsLint (used in OrionHub) and browsers catch errors.
+/*jslint browser: true*/ // This line tells jsLint that the code will run in a browser.
+
